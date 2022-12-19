bc61921350d0f5bdd17b11e1d081d306f987c9cf
avanim <axm1205@case.edu>
2022-12-18 21:02:50 -0500

    defined columns and properties for ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // foreign key that references ID in Product model
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // foreign key that references ID in Tag model
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
