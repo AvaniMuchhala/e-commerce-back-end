e687fec3533c01a206593a8b85ea92a0282263cc
avanim <axm1205@case.edu>
2022-12-18 20:52:47 -0500

    defined columns and properties for Product model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validates that value is decimal
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      // will only allow numbers
+      validate: {
+        isNumeric: true
+      }
+    },
+    // foreign key that references ID in Category model
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
