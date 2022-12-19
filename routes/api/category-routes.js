7fd682dd37db6777e50fbcb418c862644c503f38
avanim <axm1205@case.edu>
2022-12-18 22:44:00 -0500

    laid out logic foundation for each route in product-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -71,7 +71,7 @@ router.delete('/:id', async (req, res) => {
       if (categoryData) {
         res.status(200).json(categoryData);
       } else {
-        res.status(404).json({message: 'No category with this id!'});
+        res.status(404).json({message: 'No category found with this id!'});
       }
     } else {
       res.status(404).json({message: 'Category id not provided!'});
