ad4eb62449ce900a28152ee7ae23d82e791221a5
avanim <axm1205@case.edu>
2022-12-20 18:32:29 -0500

    added more checks/status codes to tag-routes, added through table to include property

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,12 +3,11 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// GET request to find all tags and include its associated Product data
 router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }]
+      include: [{ model: Product, through: ProductTag }]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -16,22 +15,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET Request to find a single tag by its `id` and include its associated Product data
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
+    // Check if ID provided in URL
     if (req.params.id) {
       const tagData = await Tag.findByPk(req.params.id, {
-        include: [{ model: Product }]
+        include: [{ model: Product, through: ProductTag }]
       });
   
+      // Check if tag with specified ID found
       if (tagData) {
         res.status(200).json(tagData);
       } else {
-        res.status(404).json({ message: 'No tag found with this ID' });
+        res.status(404).json({ message: 'No tag found with this ID!' });
       }
     } else {
-      res.status(404).json({ message: 'No tag ID provided!' });
+      res.status(400).json({ message: 'No tag ID provided!' });
     }
     
   } catch (err) {
@@ -39,47 +39,54 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// POST Request to create a new tag
 router.post('/', async (req, res) => {
-  // create a new tag
   try {
-    if (req.body.tag_id) {
+    // Check if req.body has 'tag_name' property
+    if (req.body.tag_name) {
       const tagData = await Tag.create({
-        tag_id: req.body.tag_id
+        tag_name: req.body.tag_name
       });
       res.status(200).json(tagData);
     } else {
-      res.status(400).json({ message: 'No req.body.tag_id provided!' });
+      res.status(400).json({ message: 'Please supply req.body as object with `tag_name` property.' });
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// PUT Request to update a tag's name by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
   try {
+    // Check that ID is provided in URL and tag_name is specified in req.body
     if (req.params.id) {
-      const tagData = await Tag.update(req.body, {
-        where: {
-          id: req.params.id
+      if (req.body.tag_name) {
+        const tagData = await Tag.update(req.body, {
+          where: {
+            id: req.params.id
+          }
+        });
+  
+        if (tagData[0]) {
+          res.status(200).json(tagData);
+        } else {
+          res.status(404).json({message: 'No tag found with this ID!'});
         }
-      });
-      if (tagData[0]) {
-        res.status(200).json(tagData);
-        // should i change what is sent in successful response to smth more meaningful?
+
       } else {
-        res.status(404).json({message: 'No tag with this ID!'});
+        res.status(400).json({ message: 'Please supply req.body as object with `tag_name` property.' });
       }
     } else {
-      res.status(404).json({message: 'No tag ID provided!' });
+      res.status(400).json({message: 'No tag ID provided!' });
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// DELETE Request to delete a tag by its `id` value
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
   try {
     if (req.params.id) {
       const tagData = await Tag.destroy({
@@ -87,13 +94,15 @@ router.delete('/:id', async (req, res) => {
           id: req.params.id
         }
       });
+
+      // Check if tag found with specified ID
       if (tagData) {
         res.status(200).json(tagData);
       } else {
         res.status(404).json({ message: 'No tag found with this ID!' });
       }      
     } else {
-      res.status(404).json({ message: 'No tag id provided!' });
+      res.status(400).json({ message: 'No tag ID provided!' });
     }
   } catch (err) {
     res.status(500).json(err);
